1580fc243450c75422765db15eebc0c5bc20536e
= <=>
2026-09-18 16:44:30 +0530

    making it live

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ const io = new Server(server, {
 // Start all socket listeners
 initializeSocket(io);
 
-server.listen(5000, () => {
-    console.log("Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
